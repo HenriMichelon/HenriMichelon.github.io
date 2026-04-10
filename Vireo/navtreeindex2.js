bd595f875f94d71215c6f64da369bbfbfcfdc82e
Henri Michelon <hmichelon@mailcourrier.fr>
2026-04-10 17:13:56 +0200

    Added vireo::QueryPool for GPU-side performance profiling.

---
+++ Vireo/navtreeindex2.js
@@ -0,0 +1,8 @@
+var NAVTREEINDEX2 =
+{
+"tutorial_1_13_drawing.html":[1,1,12],
+"tutorial_1_14_conclusion.html":[1,1,13],
+"unionvireo_1_1ClearValue.html":[3,0,0,4],
+"unionvireo_1_1ClearValue.html#a111ac8b82a0ae90d45d22ed6cac83824":[3,0,0,4,0],
+"unionvireo_1_1ClearValue.html#a726a4f29b4e589b659b61594ba5a0de9":[3,0,0,4,1]
+};
